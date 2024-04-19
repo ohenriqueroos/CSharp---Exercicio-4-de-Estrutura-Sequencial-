a7fe635b118c64f46a58f48c20f16bd9aeebfc66
henri <henri@HENRIQUE>
2024-04-18 23:06:17 -0300

    Add project files.

---
+++ SequentialStructureExercise4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace SequentialStructureExercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int employeeNumber = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+            double hourPayment = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salary = hourPayment * hours;
+
+            Console.WriteLine("NUMBER = " + employeeNumber);
+            Console.WriteLine("SALARY = U$ " + salary.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
